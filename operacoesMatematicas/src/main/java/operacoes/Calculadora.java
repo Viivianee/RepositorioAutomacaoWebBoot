7b1200a6606d8fe9f6e860ad1e94fc0013a9c5ef
Viivianee <vivianesouzajesus15@gmail.com>
2023-07-11 20:05:30 -0300

    subindo automacao

---
+++ operacoesMatematicas/src/main/java/operacoes/Calculadora.java
@@ -0,0 +1,26 @@
+package operacoes;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		OperacoesMatematicas op = new OperacoesMatematicas ();
+		
+		op.divisao(20,2);
+		
+		op.multiplicacao(5,20);
+		
+		op.soma(45,45);
+		
+		op.subtracao(50,25);
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ operacoesMatematicas/src/main/java/operacoes/OperacoesMatematicas.java
@@ -0,0 +1,33 @@
+package operacoes;
+
+public class OperacoesMatematicas {
+
+	public void divisao ( int n1 , int n2) {
+		int divisao = n1 / n2 ; 
+		
+		System.out.println(divisao);
+	}
+	public void multiplicacao (int n1 , int n2) {
+		int multiplicacao = n1 * n2;
+		
+		System.out.println(multiplicacao);
+	}
+		public void soma (int n1 , int n2) {
+		int soma = n1 + n2;
+		
+		System.out.println(soma);
+		}
+		public void subtracao (int n1 , int n2) {
+			int subtracao = n1 - n2;
+			
+		System.out.println(subtracao);
+		}
+		
+		}	
+		
+	
+	
+	
+	
+	
+	
